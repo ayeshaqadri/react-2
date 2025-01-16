6912609d092d9505b48ae0538855dc860a3bcf39
ayeshaqadri <qadria392@gmail.com>
2025-01-16 23:11:30 +0500

    'incomplete'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/Navbar';
+import React from "react";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <NavbarComponent />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,48 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-// function NavbarComponent() {
-//   return (
-//     <>
-//       <Navbar bg="dark" data-bs-theme="dark">
-//         <Container>
-//           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">Home</Nav.Link>
-//             <Nav.Link href="#features">Features</Nav.Link>
-//             <Nav.Link href="#pricing">Pricing</Nav.Link>
-//           </Nav>
-//         </Container>
-//       </Navbar>
-//     </>
-//   );
-// }
-
-
-
-
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import {
   MDBNavbar,
   MDBNavbarToggler,
   MDBIcon,
   MDBNavbarNav,
   MDBNavbarItem,
+  MDBContainer,
   MDBNavbarLink,
-  MDBContainer
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 
 export default function NavbarComponent() {
   return (
-    <header style={{ paddingLeft: 0 }}>
-      <MDBNavbar expand='lg' light bgColor='white'>
+    <header>
+      <MDBNavbar expand="lg" light bgColor="white">
         <MDBContainer fluid>
           <MDBNavbarToggler
-            aria-controls='navbarExample01'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <MDBIcon fas icon='bars' />
+            <MDBIcon fas icon="bars" />
           </MDBNavbarToggler>
-          <div className='collapse navbar-collapse' id='navbarExample01'>
-            <MDBNavbarNav right className='mb-2 mb-lg-0'>
-              <MDBNavbarItem active>
-                <MDBNavbarLink aria-current='page' href='#'>
+          <div className="collapse navbar-collapse" id="navbarExample01">
+            <MDBNavbarNav right className="mb-2 mb-lg-0">
+              <MDBNavbarItem>
+                <Link to="/" className="nav-link">
                   Home
-                </MDBNavbarLink>
+                </Link>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>About</MDBNavbarLink>
+                <Link to="/users" className="nav-link">
+                  Users
+                </Link>
               </MDBNavbarItem>
             </MDBNavbarNav>
           </div>
         </MDBContainer>
       </MDBNavbar>
-
-      <div
-        className='p-5 text-center bg-image'
-        style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: 400 }}
-      >
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='mb-3'>Heading</h1>
-              <h4 className='mb-3'>Subheading</h4>
-              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-                Call to action
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
     </header>
   );
 }
-// export default NavbarComponent;
