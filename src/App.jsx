9882faf68d3da0a685f3c78038f5f3321c687043
ayeshaqadri <qadria392@gmail.com>
2025-02-06 23:00:15 +0500

    'prac'

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./components/Navbar";
-import ImageCard from "./components/Cards"
+import ImageCard from "./components/Cards";
 import Home from "./components/Home";
 import About from "./components/About";
 import Users from "./components/Users";
@@ -11,21 +11,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
+      {/* Navbar */}
       <NavbarComponent />
-      <ImageCard />
-      
-<h1 class="text-3xl font-bold underline">
-Hello world!
-</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
+
+      {/* Main Content */}
+      <div className="container mt-4">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <ImageCard />
+                <h1 className="text-3xl font-bold underline mt-4">Ayesha Here!!!</h1>
+                <button
+                  className="bg-sky-950 text-slate-50 font-semibold py-2 px-6 rounded-lg shadow-md hover:bg-slate-50 hover:text-sky-950 transition-all duration-300 ease-in-out border-2 border-sky-950 hover:border-slate-400 transform hover:scale-105 mt-4"
+                  aria-label="Press me"
+                >
+                  PRESS ME
+                </button>
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
-
-
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   MDBNavbar,
   MDBNavbarToggler,
@@ -7,42 +7,54 @@ import {
   MDBNavbarNav,
   MDBNavbarItem,
   MDBContainer,
-  MDBNavbarLink,
 } from "mdb-react-ui-kit";
 
 export default function NavbarComponent() {
+  const location = useLocation(); // Get the current route location
+  console.log("NavbarComponent rendered"); // Debugging line
+
   return (
     <header>
-      <MDBNavbar expand="lg" light bgColor="white">
-        <MDBContainer fluid>
-          <MDBNavbarToggler
-            aria-controls="navbarExample01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <MDBIcon fas icon="bars" />
-          </MDBNavbarToggler>
-          <div className="collapse navbar-collapse" id="navbarExample01">
-            <MDBNavbarNav right className="mb-2 mb-lg-0">
-              <MDBNavbarItem>
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <Link to="/about" className="nav-link">
-                  About
-                </Link>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <Link to="/users" className="nav-link">
-                  Users
-                </Link>
-              </MDBNavbarItem>
-            </MDBNavbarNav>
-          </div>
-        </MDBContainer>
-      </MDBNavbar>
+<MDBNavbar
+  expand="lg"
+  light
+  bgColor="black"
+  className="text-slate-50 font-semibold"
+  style={{ border: "2px solid red" }}
+>
+  <MDBContainer fluid>
+    {/* Navbar Toggler for Mobile */}
+    <MDBNavbarToggler
+      aria-controls="navbar"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      data-target="#navbar"
+    >
+      <MDBIcon fas icon="bars" />
+    </MDBNavbarToggler>
+
+    {/* Navbar Collapse */}
+    <div className="collapse navbar-collapse" id="navbar">
+      <MDBNavbarNav right className="mb-2 mb-lg-0">
+        <MDBNavbarItem>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </MDBNavbarItem>
+        <MDBNavbarItem>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </MDBNavbarItem>
+        <MDBNavbarItem>
+          <Link to="/users" className="nav-link">
+            Users
+          </Link>
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </div>
+  </MDBContainer>
+</MDBNavbar>
     </header>
   );
 }
