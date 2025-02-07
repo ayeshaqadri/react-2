99b8fcae5bd64e8ce41b649ab3351eff9a669652
ayeshaqadri <qadria392@gmail.com>
2025-02-07 21:34:21 +0500

    'praccccc'

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
               <>
                 <Home />
                 <ImageCard />
-                <h1 className="text-3xl font-bold underline mt-4">Ayesha Here!!!</h1>
+                <h1 className="text-3xl font-bold underline mt-4">Ayesha is practicing react with the help of deepseek and chatgpt and applying tailwind and bootstrap !!!</h1>
                 <button
                   className="bg-sky-950 text-slate-50 font-semibold py-2 px-6 rounded-lg shadow-md hover:bg-slate-50 hover:text-sky-950 transition-all duration-300 ease-in-out border-2 border-sky-950 hover:border-slate-400 transform hover:scale-105 mt-4"
                   aria-label="Press me"
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>Welcome to About</div>
+    <div>About Ayesha Qadri...</div>
   )
 }
